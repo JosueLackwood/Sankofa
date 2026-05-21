594d4f4788759ad98d408775550faddb0c6eea38
JosueLackwood <135396484+JosueLackwood@users.noreply.github.com>
2026-05-21 13:49:00 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,50 @@
 import type { Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/toaster";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Sankofa",
-  description: "Modern community event platform",
+  description:
+    "Sankofa is a platform for event management. It allows you to create and manage events, sell tickets and more.",
+  icons: {
+    icon: "/images/favicon.ico",
+  },
+  openGraph: {
+    title: "Sankofa",
+    description:
+      "Sankofa is a platform for event management. It allows you to create and manage events, sell tickets and more.",
+    images: [{ url: "/images/preview.png" }],
+  },
+  creator: "Yash Yerunkar",
+  publisher: "Yash Yerunkar",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.className}`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+          <Toaster />
+          <SpeedInsights />
+        </body>
       </html>
     </ClerkProvider>
   );
